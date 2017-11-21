8f7d0e182870b70ef6559d15b0973b30815aacca
MinakovaLisa <Giti528@ya.ru>
2017-11-21 19:51:07 +0400

    Внесла изменения по упражнениям из Моргана. Добавила немного новых слов. Сделала приведение заглавной буквы к строчной. Ввела ограничение по ходам. Исправила ошибку с подсчетом попыток и оставшихся для угадывания букв + сделала сообщение о повторном введении ранее угаданной буквы.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //Создаем массив со словами
-var words = ["программа", "макака", "прекрасный", "оладушек"]
+var words = ["программа", "макака", "прекрасный", "оладушек", "солеварня", "гадалка", "мороз",
+    "совокупность", "поворот", "аквариум", "водоворот", "переправа", "корабль", "каравай", "пожарный"]
 //Выбираем случайное слово
 var word = words[Math.floor(Math.random() * words.length)]
 
@@ -10,29 +11,43 @@ for (var i = 0; i < word.length; i++) {
 }
 // Переменная с количеством оставшихся букв
 var remeiningLetters = word.length
+var attempt = 0
 
 //Пока остались неразгаданные буквы
-while (remeiningLetters > 0) {
+while (remeiningLetters > 0 && attempt < 15) {
     //Показываем текущее состояние игры
     alert(answerArray.join(" "))
     //Запрашиваем у игрока ответ
     var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.")
+
+
+    for (var i = 0; i <= word.length; i++) {
+        if (guess === answerArray[i]) {
+            alert("Эта буква ужа угадана! Введите другую букву!")
+            break
+        }
+    }
+
     if (guess === null) {
         break
     }
     else if (guess.length !== 1) {
         alert("Пожалуйста введите только одну букву.")
     }
-    //Основной код, сверка введенной буквы с имеющимис в загаданном слове
+    //Основной код, сверка введенной буквы с имеющимися в загаданном слове
     else {
         for (var j = 0; j < word.length; j++) {
-            if (word[j] === guess) {
+            //Приводим введеный символ к строчному написанию
+            guess = guess.toLowerCase()
+            if (word[j] === guess && answerArray[j] === "_") {
                 answerArray[j] = guess
                 remeiningLetters--
-
             }
+            //else alert("Эта буква уже отгадана! Введите другую букву!")
         }
     }
+    //Увеличиваем количество попыток
+    attempt++
 }
 
 alert(answerArray.join(" "))

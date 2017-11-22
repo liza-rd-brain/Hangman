92eba4647addc74caec436e38e034d4f97c54fdb
MinakovaLisa <Giti528@ya.ru>
2017-11-23 00:08:02 +0400

    Внесла изменения по книге. По максимуму использованы функциональные выражения. Пока сломалась логика замечания на повтороное введение угаданной буквы.

--- main.js
+++ main.js
@@ -2,31 +2,40 @@
 var words = ["программа", "макака", "прекрасный", "оладушек", "солеварня", "гадалка", "мороз",
     "совокупность", "поворот", "аквариум", "водоворот", "переправа", "корабль", "каравай", "пожарный"]
 //Выбираем случайное слово
-var word = words[Math.floor(Math.random() * words.length)]
+var pickWord = function () {
+    return words[Math.floor(Math.random() * words.length)]
+}
+var word = pickWord()
 
 //Итоговый массив, куда заносятся правильно угаданные буквы
-var answerArray = []
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_"
+//кусок корявый. Не понимаю, зачем все запихивать в функц. выражения.
+//Но пока все делаю по книге
+var setupAnswerArray = function (word) {
+    var emptyArray = []
+    for (var i = 0; i < word.length; i++) {
+        emptyArray[i] = "_"
+    }
+    return emptyArray
 }
+var answerArray = setupAnswerArray(word);
+
+var showPlayerProgress = function (answerArray) {
+    alert(answerArray.join(" "))
+}
+
 // Переменная с количеством оставшихся букв
-var remeiningLetters = word.length
+var remainingLetters = word.length
 var attempt = 0
 
 //Пока остались неразгаданные буквы
-while (remeiningLetters > 0 && attempt < 15) {
+while (remainingLetters > 0 && attempt < 15) {
     //Показываем текущее состояние игры
-    alert(answerArray.join(" "))
+    showPlayerProgress(answerArray)
     //Запрашиваем у игрока ответ
-    var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.")
-
-
-    for (var i = 0; i <= word.length; i++) {
-        if (guess === answerArray[i]) {
-            alert("Эта буква ужа угадана! Введите другую букву!")
-            break
-        }
+    var getGuess = function () {
+       return prompt ("Угадайте букву или нажмите Отмена для выхода из игры.")
     }
+    var guess = getGuess()
 
     if (guess === null) {
         break
@@ -34,22 +43,36 @@ while (remeiningLetters > 0 && attempt < 15) {
     else if (guess.length !== 1) {
         alert("Пожалуйста введите только одну букву.")
     }
-    //Основной код, сверка введенной буквы с имеющимися в загаданном слове
+    /*else if (guess === answerArray[i]) {
+        alert("Эта буква ужа угадана! Введите другую букву!")
+        break
+    }*/
     else {
-        for (var j = 0; j < word.length; j++) {
-            //Приводим введеный символ к строчному написанию
-            guess = guess.toLowerCase()
-            if (word[j] === guess && answerArray[j] === "_") {
-                answerArray[j] = guess
-                remeiningLetters--
+        var updateGameState = function (guess, word, answerArray) {
+            for (var j = 0; j < word.length; j++) {
+                //Приводим введеный символ к строчному написанию
+                guess = guess.toLowerCase()
+                //Повторояется одна буква в слове столько раз
+                var repeatLetter = 0
+                if (word[j] === guess && answerArray[j] === "_") {
+                    answerArray[j] = guess
+                    repeatLetter++
+                }
             }
-            //else alert("Эта буква уже отгадана! Введите другую букву!")
+            return repeatLetter
+            attempt++
         }
+        // correctGuesses: количество открытых букв
+        var correctGuesses = updateGameState(guess, word, answerArray);
+        remainingLetters -= correctGuesses;
+        //Увеличиваем количество попыток
+        attempt++
     }
-    //Увеличиваем количество попыток
-    attempt++
+
+    var showAnswerAndCongratulatePlayer = function (answerArray) {
+        alert(answerArray.join(" "))
+        alert("Отлично! Было загадано слово " + word)
+    }
 }
 
-alert(answerArray.join(" "))
-alert("Отлично! Было загадано слово " + word)
-
+showAnswerAndCongratulatePlayer(answerArray)
